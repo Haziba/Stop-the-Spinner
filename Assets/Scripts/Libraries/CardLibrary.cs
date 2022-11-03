849ea8f9bac052c4e5a41450b2ceacb2857ec581
Harry Boyes <harry.boyes@gmail.com>
2022-11-03 16:27:59 +0000

    Card performing refactor

--- Assets/Scripts/Libraries/CardLibrary.cs
+++ Assets/Scripts/Libraries/CardLibrary.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Libraries
 {
@@ -6,15 +8,101 @@ namespace Libraries
   {
     public static IDictionary<CardName, Card> Cards = new Dictionary<CardName, Card>
     {
-      [CardName.SwordThem] = new Card(2),
-      [CardName.AxeThem] = new Card(3),
-      [CardName.DaggerThem] = new Card(1), 
-      [CardName.BiteThem] = new Card(3),
-      [CardName.DistractThem] = new Card(1), 
-      [CardName.FocusMe] = new Card(1),
-      [CardName.IntoxicateThem] = new Card(2), 
-      [CardName.ShieldBashThem] = new Card(3),
-      [CardName.RaiseShield] = new Card(1)
+      [CardName.SwordThem] = new Card(2, (meState, themState) =>
+      {
+        return new SpinWheelOutcome(new SpinnerConfiguration(0.5f, 0.1f));
+      }, (spinnerResult, meState, themState) =>
+      {
+        switch (spinnerResult)
+        {
+          case SpinnerResult.Hit:
+            themState.TakeDamage(1);
+            break;
+          case SpinnerResult.Crit:
+            themState.TakeDamage(2);
+            break;
+        }
+      }),
+      [CardName.AxeThem] = new Card(3, (meState, themState) =>
+      {
+        return new SpinWheelOutcome(new SpinnerConfiguration(0.4f, 0.2f));
+      }, (spinnerResult, meState, themState) =>
+      {
+        switch (spinnerResult)
+        {
+          case SpinnerResult.Hit:
+            themState.TakeDamage(2);
+            break;
+          case SpinnerResult.Crit:
+            themState.TakeDamage(3);
+            break;
+        }
+      }),
+      [CardName.DaggerThem] = new Card(1, (meState, themState) =>
+      {
+        return new SpinWheelOutcome(new SpinnerConfiguration(0.6f, 0.03f));
+      }, (spinnerResult, meState, themState) =>
+      {
+        switch (spinnerResult)
+        {
+          case SpinnerResult.Hit:
+            themState.TakeDamage(1);
+            break;
+          case SpinnerResult.Crit:
+            themState.TakeDamage(3);
+            break;
+        }
+      }), 
+      [CardName.BiteThem] = new Card(3, (meState, themState) =>
+      {
+        return new SpinWheelOutcome(new SpinnerConfiguration(0.35f, 0.1f));
+      }, (spinnerResult, meState, themState) =>
+      {
+        switch (spinnerResult)
+        {
+          case SpinnerResult.Hit:
+            themState.TakeDamage(2);
+            break;
+          case SpinnerResult.Crit:
+            themState.TakeDamage(4);
+            break;
+        }
+      }),
+      [CardName.DistractThem] = new Card(1, (meState, themState) =>
+      {
+        themState.AddEffect(AgentStatusEffects.Distracted, 2);
+        return new NoOutcome();
+      }), 
+      [CardName.FocusMe] = new Card(1, (meState, themState) =>
+      {
+        meState.AddEffect(AgentStatusEffects.Focused, 2);
+        return new NoOutcome();
+      }),
+      [CardName.IntoxicateThem] = new Card(2, (meState, themState) =>
+      {
+        themState.AddEffect(AgentStatusEffects.Intoxicated, 2);
+        return new NoOutcome();
+      }), 
+      [CardName.ShieldBashThem] = new Card(2, (meState, themState) =>
+      {
+        return new SpinWheelOutcome(new SpinnerConfiguration(0.5f, 0.1f));
+      }, (spinnerResult, meState, themState) =>
+      {
+        switch (spinnerResult)
+        {
+          case SpinnerResult.Hit:
+            themState.TakeDamage(1);
+            break;
+          case SpinnerResult.Crit:
+            themState.TakeDamage(2);
+            break;
+        }
+      }),
+      [CardName.RaiseShield] = new Card(1, (meState, themState) =>
+      {
+        Debug.Log("Better implement this bad boy");
+        return new NoOutcome();
+      })
     };
   }
 }
@@ -22,11 +110,17 @@ namespace Libraries
 public class Card
 {
   int _manaCost;
-  
+  Func<AgentState,AgentState,ICardOutcome> _perform;
+  Action<SpinnerResult,AgentState,AgentState> _resolve;
+
   public int ManaCost => _manaCost;
+  public Func<AgentState, AgentState, ICardOutcome> Perform => _perform;
+  public Action<SpinnerResult, AgentState, AgentState> Resolve => _resolve;
   
-  public Card(int manaCost)
+  public Card(int manaCost, Func<AgentState, AgentState, ICardOutcome> perform, Action<SpinnerResult, AgentState, AgentState> resolve = null)
   {
     _manaCost = manaCost;
+    _perform = perform;
+    _resolve = resolve;
   }
 }
--- Assets/Scripts/PerformCardEffect.cs
+++ Assets/Scripts/PerformCardEffect.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-using System.Collections.Generic;
+using Libraries;
 
 public class PerformCardEffect
 {
@@ -20,83 +20,15 @@ public class PerformCardEffect
 
   public ICardOutcome Perform()
   {
-    switch(_cardName)
-    {
-      case CardName.SwordThem:
-        return new SpinWheelOutcome(SpinnerConfig(CardName.SwordThem));
-      case CardName.AxeThem:
-        return new SpinWheelOutcome(SpinnerConfig(CardName.AxeThem));
-      case CardName.DaggerThem:
-        return new SpinWheelOutcome(SpinnerConfig(CardName.DaggerThem));
-      case CardName.BiteThem:
-        return new SpinWheelOutcome(SpinnerConfig(CardName.BiteThem));
-      case CardName.DistractThem:
-        _themState.AddEffect(AgentStatusEffects.Distracted, 2);
-        return new NoOutcome();
-      case CardName.FocusMe:
-        _meState.AddEffect(AgentStatusEffects.Focused, 2);
-        return new NoOutcome();
-      case CardName.IntoxicateThem:
-        _themState.AddEffect(AgentStatusEffects.Intoxicated, 2);
-        return new NoOutcome();
-    }
-
-    return new NoOutcome();
+    return CardLibrary.Cards[_cardName].Perform(_meState, _themState);
   }
-
-  SpinnerConfiguration SpinnerConfig(CardName cardName)
-  {
-    switch(cardName)
-    {
-      case CardName.SwordThem:
-        return new SpinnerConfiguration(0.5f, 0.1f);
-      case CardName.AxeThem:
-        return new SpinnerConfiguration(0.4f, 0.2f); 
-      case CardName.DaggerThem:
-        return new SpinnerConfiguration(0.6f, 0.03f);
-      case CardName.BiteThem:
-        return new SpinnerConfiguration(0.35f, 0.1f);
-      default:
-        return new SpinnerConfiguration(0.5f, 0.1f);
-    }
-  }
-
-  // todo: This shouldn't live here lol
-  IDictionary<CardName, CardDamage> _cardDamages = new Dictionary<CardName, CardDamage>
-  {
-    [CardName.SwordThem] = new CardDamage(1, 2),
-    [CardName.AxeThem] = new CardDamage(2, 3),
-    [CardName.DaggerThem] = new CardDamage(1, 2),
-    [CardName.BiteThem] = new CardDamage(2, 4),
-  };
 
   public void ResolveSpinner(SpinnerResult result)
   {
-    if(result == SpinnerResult.Hit) {
-      _themState.TakeDamage(_cardDamages[_cardName].Hit);
-      _themHealthBar.GetComponent<HealthBarController>().SetHealth(_themState.Health());
-    }
-    if(result == SpinnerResult.Crit) {
-      _themState.TakeDamage(_cardDamages[_cardName].Crit);
-      _themHealthBar.GetComponent<HealthBarController>().SetHealth(_themState.Health());
-    }
+    CardLibrary.Cards[_cardName].Resolve(result, _meState, _themState);
+    _meHealthBar.GetComponent<HealthBarController>().SetHealth(_meState.Health());
+    _themHealthBar.GetComponent<HealthBarController>().SetHealth(_themState.Health());
   }
-}
-
-// todo: Move all these classes to sensible locations, maybe scoped to a folder / namespace
-public class CardDamage
-{
-  int _hit;
-  int _crit;
-
-  public CardDamage(int hit, int crit)
-  {
-    _hit = hit;
-    _crit = crit;
-  }
-
-  public int Hit => _hit;
-  public int Crit => _crit;
 }
 
 public interface ICardOutcome
